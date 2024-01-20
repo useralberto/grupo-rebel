5cdf2e76c03a3d48e624ad545de5a7e01852bbf7
useralberto <lalonso@thebrandindustry.com>
2024-01-19 18:22:19 -0600

    WORKING ON: Configured directories and files in templates...

--- web/assets/js/components/mastHead.component.js
+++ web/assets/js/components/mastHead.component.js
@@ -1,8 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!****************************************************!*\
-  !*** ./templates/components/mastHead/src/index.js ***!
-  \****************************************************/
 console.log("Holas");
-/******/ })()
-;
